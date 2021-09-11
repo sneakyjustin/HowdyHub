26132327e3dbd96aefd1d00bf6dbb7b7205a476e
NaifBhuiyan <30419422+NaifBanana@users.noreply.github.com>
2021-09-11 17:20:41 -0500

    Course Search
    Courses can now be searched through GET request parameters.

--- Hello2.py
+++ Hello2.py
@@ -1,15 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import json
 
 
 app = Flask(__name__)
-
-
-f = open("dummy.json")
-
-all_courses = json.load(f)
-
-f.close()
 
 @app.route("/")
 
@@ -18,8 +11,26 @@ def home():
     return render_template('home.html', title = 'Home')
 
 @app.route("/course")
-def course():
-    return render_template('course.html', posts = all_courses["classes"], title = 'Course')
+def course_search_list():
+    req_dep = request.args.get("dep")
+    if req_dep is not None:
+        req_dep = req_dep.upper()
+    req_course = request.args.get("crs_num")
+    req_instr = request.args.get("instr")
+    if req_instr is not None:
+        req_instr = req_instr.capitalize()
+    f = open("dummy.json")
+    all_courses = json.load(f)
+    f.close()
+
+    courses = []
+    for i in all_courses["classes"]:
+        if (req_dep is None or i["department"] == req_dep):
+            if (req_course is None or i["course"] == req_course):
+                if (req_instr is None or i["instructor"] == req_instr):
+                    courses.append(i)
+
+    return render_template('course.html', posts = courses, title = 'Course')
 
 
 
